51d03e7c49c9dd7b51e17a32094d2cafb9a780cc
Duck0-Land <erhilston@gmail.com>
2023-06-14 20:14:26 -0700

    added variables to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//vairiables
+var personalKey = "361c6a6d2ca17dff9e6e6c600d7e1a7a";
+var citySearch;
+var citiesSearched;
+var citiesStorageArray = [];
+var fiveDayforecast = "https://api.openweathermap.org/data/2.5/onecall?";
